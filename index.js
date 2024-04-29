7110f3b3fbbb3442c0587d5c760872d18fe47053
Michael Giannini <giannini.michaelg@gmail.com>
2024-04-29 17:25:25 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length || pos < 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.at(pos);
+    }
+  }
 
-  max() {}
+  max() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      this.items.sort((a, b) => a -b);
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() { 
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList")
+    } else {
+      this.items.sort((a, b) => a -b);
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() { 
+    let sum = 0;
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      this.items.forEach((num) => {
+        sum = sum + num;
+      })
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      const sum = this.sum();
+      return sum/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
